2bcbd6c256695919656f4cea7d0b5ca2cd6b0d18
Brandon Shar <brandon.shar@icloud.com>
2022-07-22 15:45:52 -0400

    Add more required ven implementations with signatures

--- ven.py
+++ ven.py
@@ -1,9 +1,6 @@
-from datetime import timedelta
+from datetime import timedelta, datetime
 from openleadr import OpenADRClient
 from random import random
-
-def example_report_value():
-  return random()
 
 class ExampleVen:
   def __init__(self, **kwargs):
@@ -16,13 +13,49 @@ class ExampleVen:
     self.client.add_handler('on_event', self.handle_event)
 
     self.client.add_report(
-      callback=example_report_value,
+      callback=self.energy_report,
       report_specifier_id='RealEnergy',
       resource_id='example-device-001',
-      measurement='voltage',
-      sampling_rate=timedelta(seconds=10),
+      sampling_rate=timedelta(hours=1),
     )
 
+    self.client.add_report(
+      callback=self.status_report,
+      report_specifier_id='Status',
+      resource_id='example-device-001',
+      sampling_rate=timedelta(seconds=30),
+    )
+
+  
   @staticmethod
   async def handle_event(event):
     print('Just received an event!', event)
+
+    for signal in event['event_signals']:
+      if signal['signal_name'] == 'ELECTRICITY_PRICE' and signal['signal_type'] == 'price':
+        for interval in signal['intervals']:
+          print(f'Start: {interval["dtstart"].isoformat()}')
+          print(f'Duration: {interval["duration"].seconds}')
+          print(f'Price: {interval["signal_payload"]}')
+
+  @staticmethod
+  async def energy_report():
+    """
+    OpenLeadr will automatically aggregate values here, so we only need
+    to pass a measurement. Alternatively, you can pass a list of (datetime, value) tuples
+    to process all of the report at once
+    """
+    return random()
+  
+  @staticmethod
+  async def status_report():
+    """
+    The tuple expected is (online, manual_override, capacity). Just as with energy, a datetime value
+    can be added as the first element of the tuple if you would like to determine it manually.
+    """
+    return (True, False, {
+      'min': 0,
+      'max': 10,
+      'current': 5,
+      'normal': 8,
+    })
